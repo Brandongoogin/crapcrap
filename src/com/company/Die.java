36548d7a6fd6cad90683f89b58024ecd9fa7a07b
brandon <brandonmarion@billericak12.com>
2019-11-13 12:27:16 -0500

    ipp

--- src/com/company/Die.java
+++ src/com/company/Die.java
@@ -1,10 +1,27 @@
 package com.company;
 
-public class Die {
+public class Die{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
         Die die = new Die();
         die.roll();
-        System.out.println)(die.getNumDots());
+        System.out.println(die.getNumDots());
     }
 }
 
